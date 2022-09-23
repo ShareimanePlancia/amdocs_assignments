56bae643286c255648eb1960cecb1b04bd3ea75d
shareimp <shareimp@YULIAMAK01.corp.amdocs.com>
2022-09-23 14:12:48 +0800

    Assignment EPortal Sept.23,2022

---
+++ src/lab/exercises/day7/Course.java
@@ -0,0 +1,21 @@
+package lab.exercises.day7;
+
+public class Course {
+	int id;
+    String name;
+    String course;
+	
+    public Course(int id, String name, String course) {
+		super();
+		this.course = course;
+	}
+	
+	public Course() {
+		
+	}
+	public String coursesDetails() {
+		return  " Student Course: "+course;
+	}	
+
+}
+
---
+++ src/lab/exercises/day7/Students.java
@@ -0,0 +1,24 @@
+package lab.exercises.day7;
+
+public class Students {
+	int id;
+    String name;
+    String course;
+    
+    public Students(){
+    	
+    }
+	
+    public Students(int id, String name, String course) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.course = course;
+	}
+
+	@Override
+    public String toString() {
+		return "Student ID: " + id + ", Student Name: " + name + " Student Course: "+course;
+	}	
+	    
+}
